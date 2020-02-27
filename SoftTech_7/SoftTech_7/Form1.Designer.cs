7df89ae1e48992c339f6342410c7b4f46dec2499
unicoded <44432228+unicoded@users.noreply.github.com>
2020-02-27 13:34:28 +0200

    Add files via upload

---
+++ SoftTech_7/SoftTech_7/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace SoftTech_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNewNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToLabelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToFIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(634, 397);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(626, 368);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(6, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 308);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(626, 368);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNewNameToolStripMenuItem,
+            this.showNameToolStripMenuItem,
+            this.addToLabelToolStripMenuItem,
+            this.loadFileToolStripMenuItem,
+            this.saveToFIleToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // addNewNameToolStripMenuItem
+            // 
+            this.addNewNameToolStripMenuItem.Name = "addNewNameToolStripMenuItem";
+            this.addNewNameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addNewNameToolStripMenuItem.Text = "Add New Name";
+            this.addNewNameToolStripMenuItem.Click += new System.EventHandler(this.addNewNameToolStripMenuItem_Click);
+            // 
+            // showNameToolStripMenuItem
+            // 
+            this.showNameToolStripMenuItem.Name = "showNameToolStripMenuItem";
+            this.showNameToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.showNameToolStripMenuItem.Text = "Show Name";
+            this.showNameToolStripMenuItem.Click += new System.EventHandler(this.showNameToolStripMenuItem_Click);
+            // 
+            // addToLabelToolStripMenuItem
+            // 
+            this.addToLabelToolStripMenuItem.Name = "addToLabelToolStripMenuItem";
+            this.addToLabelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.addToLabelToolStripMenuItem.Text = "Add to Label";
+            this.addToLabelToolStripMenuItem.Click += new System.EventHandler(this.addToLabelToolStripMenuItem_Click);
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            this.loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            this.loadFileToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.loadFileToolStripMenuItem.Text = "Load File";
+            this.loadFileToolStripMenuItem.Click += new System.EventHandler(this.loadFileToolStripMenuItem_Click);
+            // 
+            // saveToFIleToolStripMenuItem
+            // 
+            this.saveToFIleToolStripMenuItem.Name = "saveToFIleToolStripMenuItem";
+            this.saveToFIleToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToFIleToolStripMenuItem.Text = "Save To FIle";
+            this.saveToFIleToolStripMenuItem.Click += new System.EventHandler(this.saveToFIleToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(652, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(671, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Фамилия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(652, 187);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 5;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(818, 497);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(818, 497);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Картотека";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNewNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToFIleToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem showNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToLabelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ SoftTech_7/SoftTech_7/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SoftTech_7
+{
+    public partial class Form1 : Form
+    {
+        string alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЬЮЯ";
+        string[] bits;
+        string tempname;
+        string tempadress = "";
+        List<string> mylist;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Clear();
+
+            string file_path = @"C:\Users\Ivan Vasilev\source\repos\SoftTech_7\SoftTech_7\Names.txt";
+            
+
+            using (TextReader reader = File.OpenText(file_path))
+            {
+                string text = reader.ReadLine();
+                bits = text.Split(' ');
+            }
+
+
+            listBox1.Sorted = true;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                string title = alphabet.Substring(i, 1);
+                TabPage myTabPage = new TabPage(title);
+                myTabPage.Name = title;
+                tabControl1.TabPages.Add(myTabPage);
+                ListView lb = new ListView();
+                lb.Size = new System.Drawing.Size(322, 388);
+                lb.Columns.Add("Фамилия", 150, HorizontalAlignment.Left);
+                lb.Columns.Add("Адрес", 150, HorizontalAlignment.Left);
+                lb.GridLines = true;
+                for(int j = 0; j < bits.Length; j++)
+                {
+                    if (bits[j].StartsWith(alphabet.Substring(i,1)))
+                    {
+                        tempname = bits[j].ToString();
+                        string[] row = { tempname, tempadress };
+                        var listViewItem = new ListViewItem(row);
+                        lb.Items.Add(listViewItem);
+                    }
+                }
+                lb.Sort();
+                lb.View = View.Details;
+                myTabPage.Controls.Add(lb);
+            }
+            
+        }
+
+        private void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string file_path = "";
+            OpenFileDialog fd = new OpenFileDialog();
+
+            if (fd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                file_path = fd.FileName;
+            }
+
+
+            using (TextReader reader = File.OpenText(file_path))
+            {
+                string text = reader.ReadLine();
+                bits = text.Split(' ');
+            }
+
+            FillTable();
+        }
+
+        private void FillTable()
+        {
+            tabControl1.TabPages.Clear();
+
+            listBox1.Sorted = true;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                string title = alphabet.Substring(i, 1);
+                TabPage myTabPage = new TabPage(title);
+                myTabPage.Name = title;
+                tabControl1.TabPages.Add(myTabPage);
+                ListView lb = new ListView();
+                lb.Size = new System.Drawing.Size(322, 388);
+                lb.Columns.Add("Фамилия", 150, HorizontalAlignment.Left);
+                lb.Columns.Add("Адрес", 150, HorizontalAlignment.Left);
+                lb.GridLines = true;
+                for (int j = 0; j < bits.Length; j++)
+                {
+                    if (bits[j].StartsWith(alphabet.Substring(i, 1)))
+                    {
+                        tempname = bits[j].ToString();
+                        string[] row = { tempname, tempadress };
+                        var listViewItem = new ListViewItem(row);
+                        lb.Items.Add(listViewItem);
+                    }
+                }
+                lb.Sort();
+                lb.View = View.Details;
+                myTabPage.Controls.Add(lb);
+            }
+        }
+
+        private void saveToFIleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.OpenOrCreate))
+                
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    for(int i = 0; i < bits.Length; i++)
+                    {
+                        sw.Write(bits[i] + " ");
+                    }
+                }
+            }
+        }
+
+        private void addNewNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                string extra_name = textBox1.Text;
+                Array.Resize(ref bits, bits.Length + 1);
+                bits[bits.Length - 1] = extra_name;
+                FillTable();
+            }
+            else
+            {
+                MessageBox.Show("Моля въведете Фамилия в текстувата котия!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void showNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                mylist = bits.ToList();
+
+                if (mylist.Contains(textBox1.Text))
+                {
+                    string temp = textBox1.Text.Substring(0,1);
+
+                    this.tabControl1.SelectTab(temp);
+
+                }
+                else
+                {
+                    MessageBox.Show("Не съществува такава Фамилия!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Моля въведете Фамилия в текстувата котия!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void addToLabelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            mylist = bits.ToList();
+
+            if (textBox1.Text.Length > 0)
+            {
+                if (mylist.Contains(textBox1.Text))
+                {
+                    label2.Text = textBox1.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Не съществува такава Фамилия!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
